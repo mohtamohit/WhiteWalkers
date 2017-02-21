0b32a91bc5b33172eeb4c897773074f186fef6ec
Ranjith1011 <ranjith515261@gmail.com>
2017-02-21 17:31:55 +0530

    Door_animations_added

---
+++ village/Assets/Scripts/GameObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObject : MonoBehaviour
+{
+    private Animator _animator = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_animator.GetBool("isOpen"));
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        
+        _animator.SetBool("isOpen", true);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        _animator.SetBool("isOpen", false);
+    }
+}
---
+++ village/Assets/Scripts/doorsOpen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class doorsOpen : MonoBehaviour {
+    public int doorsOpened;
+    public Text doorCount;
+	// Use this for initialization
+	void Start () {
+        doorsOpened = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string s = doorsOpened.ToString();
+        doorCount.text = "Doors Opened are : " + s;
+        Debug.Log(doorsOpened);
+	}
+    void OnTriggerEnter(Collider collider)
+    {
+        doorsOpened++;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+       
+    }
+}
