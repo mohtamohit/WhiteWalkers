a8c4c0c185ac846bfdda381c066c8157d7e761ec
Mohit Mohta <mohtamohit@gmail.com>
2017-02-26 16:51:02 +0530

    Added First Iteration of Code

---
+++ First Iteration of Code/Assets/Scripts/timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+
+    public Text Timer;
+    public float TimeLeft, StartTime;
+    public float Health;
+	// Use this for initialization
+	void Start () {
+        StartTime = Time.time; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      float t = Time.time - StartTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = ((int)(t % 60)).ToString();
+        Timer.text =" Time : "+ minutes + " : " + seconds;
+	}
+}
