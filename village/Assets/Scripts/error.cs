b0ae985c1dab62bd2d5570c37f8222e7f682de4a
Ranjith1011 <ranjith515261@gmail.com>
2017-03-13 16:47:47 +0530

    Just Minor changes

---
+++ village/Assets/Scripts/error.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class error : MonoBehaviour 
+{
+    private Animator _animator = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_animator.GetBool("isOpen"));
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+
+        _animator.SetBool("isOpen", true);
+
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        _animator.SetBool("isOpen", false);
+    }
+}
